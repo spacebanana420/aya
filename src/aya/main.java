fe84224f6048260177be297367c40e7ff17fa6c6
Space Banana <tomasbessa@protonmail.com>
2024-11-05 23:23:21 +0000

    Verbosity check

--- src/aya/main.java
+++ src/aya/main.java
@@ -4,11 +4,14 @@ import aya.cli.parser;
 import aya.cli.help;
 
 public class main {
+  public static byte verbosity_level;
+  
   public static void main(String[] args) {
     if (parser.hasArgument(args, "-h")) {
       System.out.println(help.getHelp());
       return;
     }
+    verbosity_level = getVerbosityLevel(args);
     if (!systemSupported()) {
       System.out.println("Aya does not support this operating system! Aya must run under an operating system that can run an X11-based environment!");
       return;
@@ -19,5 +22,12 @@ public class main {
   private static boolean systemSupported() {
     String os = System.getProperty("os.name").toLowerCase();
     return (!os.contains("Windows") && !os.contains("mac") && !os.equals("haiku"));
+  }
+
+  private static byte getVerbosityLevel(String[] args) {
+    if (parser.hasArgument(args, "-quiet")) {return 0;}
+    else if (parser.hasArgument(args, "-verbose")) {return 2;}
+    else if (parser.hasArgument(args, "-debug")) {return 3;}
+    else {return 1;}
   }
 }
