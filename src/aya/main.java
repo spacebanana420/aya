44b001e9704e31b4b4db7baf1c6608c0ee2ab546
Space Banana <tomasbessa@protonmail.com>
2024-11-05 21:39:41 +0000

    Help screen

--- src/aya/main.java
+++ src/aya/main.java
@@ -1,7 +1,14 @@
 package aya;
 
+import aya.cli.parser;
+import aya.cli.help;
+
 public class main {
   public static void main(String[] args) {
+    if (parser.hasArgument(args, "-h")) {
+      System.out.println(help.getHelp());
+      return;
+    }
     new screenshotter().snap(args);
   }
 }
