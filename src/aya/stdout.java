40669eb731fc7b59e1a56868f87d0b1cf47d879f
Space Banana <tomasbessa@protonmail.com>
2024-11-13 20:11:04 +0000

    stdout.java expansion

--- src/aya/stdout.java
+++ src/aya/stdout.java
@@ -1,11 +1,17 @@
 package aya;
 
+import java.util.ArrayList;
+
 public class stdout {
   public static void print(String message) {
     if (global.verbosity_level > 0) {System.out.println(message);}
   }
 
   public static void print(String title, String[] contents) {
+    if (global.verbosity_level > 0) {printSeq(title, contents);}
+  }
+
+  public static void print(String title, ArrayList<String> contents) {
     if (global.verbosity_level > 0) {printSeq(title, contents);}
   }
 
@@ -17,6 +23,10 @@ public class stdout {
     if (global.verbosity_level > 1) {printSeq(title, contents);}
   }
 
+  public static void print_verbose(String title, ArrayList<String> contents) {
+    if (global.verbosity_level > 1) {printSeq(title, contents);}
+  }
+  
   public static void print_debug(String message) {
     if (global.verbosity_level > 2) {System.out.println(message);}
   }
@@ -25,10 +35,22 @@ public class stdout {
     if (global.verbosity_level > 2) {printSeq(title, contents);}
   }
 
+  public static void print_debug(String title, ArrayList<String> contents) {
+    if (global.verbosity_level > 2) {printSeq(title, contents);}
+  }
+
+  
   private static void printSeq(String title, String[] contents) {
     String txt = title;
     for (String c : contents) {txt += "\n  * " + c;}
     
+    System.out.println(txt);
+  }
+
+  private static void printSeq(String title, ArrayList<String> contents) {
+    String txt = title;
+    for (int i = 0; i < contents.size(); i++) {txt += "\n  * " + contents.get(i);}
+
     System.out.println(txt);
   }
 }
