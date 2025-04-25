71be41b07594e7ef61715f767e3b77dfc47cd7cd
Space Banana <tomasbessa@protonmail.com>
2025-04-25 01:36:37 +0100

    Remove unused code

--- src/aya/wrapper/process.java
+++ src/aya/wrapper/process.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import aya.stdout;
 
 public class process {
-  public static int run(String[] args, boolean silent) {
+  public static int run(ArrayList<String> args, boolean silent) {
     stdout.print_debug("Running command:", args);
     try {
       var pb = new ProcessBuilder(args);
@@ -15,7 +15,7 @@ public class process {
         pb.redirectOutput(Redirect.DISCARD);
         pb.redirectError(Redirect.DISCARD);
       }
-      var p = pb.start();
+      Process p = pb.start();
       p.waitFor();
       return p.exitValue();
     }
@@ -37,41 +37,9 @@ public class process {
     catch (InterruptedException e) {return null;}
   }
 
-  public static int run(ArrayList<String> args, boolean silent) {
-    return run(args.toArray(new String[0]), silent);
-  }
-
-  public static String[] concatArgs(String[]... args) {
-    if (args.length == 0 || args[0].length == 0) {return new String[0];}
-    int len = 0;
-    int i = 0;
-    for (String[] a : args) {len += a.length;}
-    String[] final_args = new String[len];
-    for (String[] a : args) {
-      for (String arg : a) {
-        final_args[i] = arg;
-        i++;
-      }
-    }
-    return final_args;
-  }
-
-  public static String[] appendArg(String[] args, String newarg) {
-    String[] newArgs = new String[args.length+1];
-    for (int i = 0; i < args.length; i++) {
-      newArgs[i] = args[i];
-    }
-    newArgs[args.length] = newarg;
-    return newArgs;
-  }
-
   public static ArrayList<String> mkList(String[] args) {
     var list = new ArrayList<String>();
     for (String a : args) {list.add(a);}
     return list;
-  }
-
-  public static void concatList(ArrayList<String> list, String[] args) {
-    for (String a : args) {list.add(a);}
   }
 }
