f80cccf5737912338b26baeefcb010362cadba84
Space Banana <tomasbessa@protonmail.com>
2025-09-30 13:52:04 +0100

    Further GUI tests

--- test/gui.java
+++ test/gui.java
@@ -1,20 +1,31 @@
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.BoxLayout;
 
 public class gui {
   public static void main(String[] args) {
     Frame window = new Frame("Aya GUI Test");
-    window.setLayout(new FlowLayout());
-    window.setSize(500, 160);
+    Panel panel = new Panel();
+    //window.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+    window.setResizable(false);
+    //window.setSize(1000, 160);
 
-    Font font = new Font("SansSeirf", Font.PLAIN, 32);
+    Font message_font = new Font("SansSeirf", Font.PLAIN, 32);
+    Font button_font = new Font("SansSeirf", Font.PLAIN, 24);
     Label message = new Label("Sample status message");
-    message.setFont(font);
+    message.setFont(message_font);
     Button button = new Button("Got it");
+    button.setFont(button_font);
+    //button.setSize(10,120);
+    button.setMaximumSize(new Dimension(100, button.getPreferredSize().height));
     
 
-    window.add(message);
-    window.add(button);
+    //window.add(message);
+    //window.add(button);
+    panel.add(message);
+    panel.add(button);
+    window.add(panel);
     window.pack();
     window.setVisible(true);
   }
