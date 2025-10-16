73db20dae3703eb247f5e3ab5e6822748584b155
Space Banana <tomasbessa@protonmail.com>
2025-10-16 18:41:54 +0100

    Version change

--- src/aya/cli/help.java
+++ src/aya/cli/help.java
@@ -1,7 +1,7 @@
 package aya.cli;
 
 public class help {
-  public static final String VERSION = "0.11";
+  public static final String VERSION = "0.11.1";
   
   public static String getHelp() {
     return
