8301faa78ad8f482b741d7666497483052e61a5f
Space Banana <tomasbessa@protonmail.com>
2026-05-25 15:59:06 +0100

    getExtension() improvements

--- src/aya/misc.java
+++ src/aya/misc.java
@@ -1,5 +1,6 @@
 package aya;
 
+//Generic functions used by some other part of the program
 public class misc {
   public static void sleep(int delay) {
     try {Thread.sleep(delay);}
@@ -20,16 +21,17 @@ public class misc {
     return true;
   }
 
+  //Retrieve the extension of a filename by finding where the "." character is
   public static String getExtension(String name) {
-    if (name.length() < 3) return ""; //1 char name + . + 1 char extension (minimum)
+    if (name.length() < 3) return ""; //1 char name + . + 1 char extension is the minimum for a filename
     int start = -1;
     for (int i = name.length()-1; i >= 0; i--) {if (name.charAt(i) == '.') {start = i; break;}}
     if (start == -1 || start == name.length()-1) return "";
    
-     start++; //skips the . character
-    String extension = "";
-    for (int i = start; i < name.length(); i++) {extension += name.charAt(i);}
-    return extension;
+    start++; //skips the . character
+    var extension = new StringBuilder();
+    for (int i = start; i < name.length(); i++) {extension.append(name.charAt(i));}
+    return extension.toString();
   }
 
   public static boolean isWorkingDirectory(String dir) {
